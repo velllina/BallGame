4d1e5c2544872aa56f6bd671a79e25b3d66d1268
Valyo <Valyo@VALYO-PC>
2016-09-27 14:56:02 +0300

    change direction and hight elevation

--- BallGame/BallGame/Form1.cs
+++ BallGame/BallGame/Form1.cs
@@ -12,25 +12,29 @@ namespace BallGame
 {
     public partial class Form1 : Form
     {
-        public int top = 300;
-        
+        public int top = 50;
+        int speed = 4;
+        int max = 50;       // height elevation
+               
         public Form1()
         {
-            InitializeComponent();
-            
+            InitializeComponent();            
         }
 
-        public void panel_Paint(object sender, PaintEventArgs e)
+         public void timer1_Tick(object sender, EventArgs e)
         {
+            top = top + speed;
+            speed += 10;
             
-            e.Graphics.FillEllipse(Brushes.ForestGreen , new Rectangle(top,panel.Left + 50,50,50));
-        }
 
-        public void timer1_Tick(object sender, EventArgs e)
-        {
-            top += 10;
+            if (top >= 400 || top <= max )
+            {
+                speed = -speed ;  // change direction
+                max += 15;        // change height elevation
+            }         
+                     
             Invalidate();
-        }
+         }
 
         public void btn1_Click(object sender, EventArgs e)
         {
@@ -42,5 +46,9 @@ namespace BallGame
             timer1.Enabled = false;
         }
 
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(Brushes.ForestGreen, new Rectangle(400, top, 50, 50));
+        }
     }
 }
